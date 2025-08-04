8bd6e3018ae9534ff17479174eedc3c7d0c1deed
matsumoto-syo <Coco586@outlook.jp>
2025-08-04 10:39:42 +0900

    ボタンが大きくなるようになった

--- Assets/Scripts/SceneChanger.cs
+++ Assets/Scripts/SceneChanger.cs
@@ -2,10 +2,27 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 public class SceneChanger : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    public void ChangeToScene(string sceneName)
+    public Animator buttonAnimator;      // ← アニメーション再生用
+    public float animationDelay = 0.5f;  // ← アニメーション再生時間
 
+    private string nextSceneName;
+
+    public void ChangeToScene(string sceneName)
     {
-        SceneManager.LoadScene(sceneName);
+        nextSceneName = sceneName;
+
+        // アニメーション再生（Trigger "Pressed" を送る）
+        if (buttonAnimator != null)
+        {
+            buttonAnimator.SetTrigger("Pressed");
+        }
+
+        // 一定時間待ってからシーン遷移
+        Invoke(nameof(LoadScene), animationDelay);
+    }
+
+    private void LoadScene()
+    {
+        SceneManager.LoadScene(nextSceneName);
     }
 }
